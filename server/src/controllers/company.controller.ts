02b7f0583b32c7b13c9f3bf11534e24290376026
Haris Sattar <harissattar8@gmail.com>
2023-07-13 13:15:42 -0500

    Deleted useless code

--- server/src/controllers/company.controller.ts
+++ server/src/controllers/company.controller.ts
@@ -1,15 +1,8 @@
 import { Request, Response } from "express";
 import companyModel, { Company } from "../models/company.model";
-import { request } from "http";
 
 export const getCompanies = async (req: Request, res: Response) => {
   const companies = await companyModel.find({});
-
-  const newCompany: Company = {
-    name: "Apple",
-    booth: "A2",
-    companyLines: [],
-  };
 
   res.json(companies);
 };
