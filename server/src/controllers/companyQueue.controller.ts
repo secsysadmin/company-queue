2c9def17e9e543d7c9e7c0500c93bfccf02ffff7
Haris Sattar <harissattar8@gmail.com>
2023-07-24 19:24:21 -0500

    Leave queue and notify next batch

--- server/src/controllers/companyQueue.controller.ts
+++ server/src/controllers/companyQueue.controller.ts
@@ -47,20 +47,54 @@ export const joinQueue = async (req: Request, res: Response) => {
     studentsInLine: updatedLine,
   };
 
-  console.log(updatedQueue);
-
   //add them to queue
-  await companyQueueModel.findByIdAndUpdate(correctQueue._id, updatedQueue);
+  // await companyQueueModel.findByIdAndUpdate(correctQueue._id, updatedQueue);
+
+  //update the queue
+  await companyQueueModel.findOneAndUpdate(
+    { _id: correctQueue._id },
+    { $set: { studentsInLine: updatedLine } }
+  );
 
   return res.json(updatedQueue.studentsInLine);
 };
 
 export const leaveQueue = async (req: Request, res: Response) => {
-  const { ticketNumber } = req.query;
+  const ticketNumber = req.query.ticketNumber;
 
   //get company, line, and student from ticketNumber
+  // const companyQueue = await companyQueueModel.find({
+  //   ticketNumber: ticketNumber,
+  // });
 
+  const companyQueue = await companyQueueModel.findOne({
+    "studentsInLine.ticketNumber": ticketNumber,
+  });
+
+  if (!companyQueue) {
+    // If the company queue is not found, the student is not in any company queue
+    return res
+      .status(404)
+      .json({ message: "Student not found in any company queue." });
+  }
+
+  const studentIndex = companyQueue.studentsInLine.findIndex(
+    (student) => student.ticketNumber === ticketNumber
+  );
+
+  if (studentIndex === -1) {
+    // If the student is not found in the company queue, return an error
+    return res
+      .status(404)
+      .json({ message: "Student not found in the company queue." });
+  }
   //remove student
+  companyQueue.studentsInLine.splice(studentIndex, 1);
+
+  await companyQueue.save();
+
+  // return res.json({ message: "Student removed from the company queue." });
+  return res.json(companyQueue.studentsInLine);
 };
 
 export const spokenTo = async (req: Request, res: Response) => {
@@ -70,7 +104,26 @@ export const spokenTo = async (req: Request, res: Response) => {
 export const notifyNext = async (req: Request, res: Response) => {
   const { companyName, lineNumber } = req.query;
 
+  const correctQueue = await companyQueueModel.find({
+    companyName: companyName,
+    lineNumber: lineNumber,
+  });
+
   //get next 5 students in line
+  const studentsInLine = correctQueue[0].studentsInLine.sort(
+    (student1, student2) => {
+      return student1.index - student2.index;
+    }
+  );
+
+  let studentsToNotify = [];
+
+  for (let i = 0; i < Math.min(5, studentsInLine.length); i++) {
+    studentsToNotify.push(studentsInLine[i].phoneNumber);
+  }
+
+  res.send(studentsToNotify);
 
   //text 5 students
+  //Deal with texting them later
 };
