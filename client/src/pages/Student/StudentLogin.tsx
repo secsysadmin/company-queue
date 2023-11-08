926a568424f38ab566c4fd22b11a89ddeb82683e
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-11-08 13:54:53 -0600

    add form verification to studentlogin

--- client/src/pages/Student/StudentLogin.tsx
+++ client/src/pages/Student/StudentLogin.tsx
@@ -1,49 +1,56 @@
 import Banner from "../../components/Banner";
-import { Input, Select, Button } from '@chakra-ui/react';
+import { Input, Select, Button, Stack } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 
 export default function StudentLogin() {
+    const [major, setMajor] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [canSubmit, setCanSubmit] = useState(false);
+
+    useEffect(() => {
+        setCanSubmit(isValidPhone(phoneNumber) && !!major);
+    }, [major, phoneNumber]);
+
     return (
-        <div style={{display:"flex", flexDirection: 'column', alignItems:'center'}}>
+        <div style={{ display: "flex", flexDirection: 'column', alignItems: 'center' }}>
             <Banner title="Queue Login"></Banner>
-            {/*@ts-ignore*/}
-            <div style={loginFormStyle}>
-                <Input style={{marginBottom: '1em'}} placeholder="Phone Number" variant='filled'></Input>
-                <Select placeholder='Major' variant='filled' style={{marginBottom: '1em'}}>
+            <Stack marginTop='2em' spacing={3}>
+                <Input placeholder="Phone Number" variant='filled' onChange={(ev) => setPhoneNumber(ev.target.value)}></Input>
+                <Select placeholder='Major' variant='filled' onChange={(ev) => setMajor(ev.target.value)}>
                     <option value="AERO">AERO</option>
-                    <option value="AREN">CSCE</option>          
+                    <option value="AREN">AREN</option>
                     <option value="BAEN">BAEN</option>
                     <option value="BMEN">BMEN</option>
                     <option value="CHEN">CHEN</option>
-                    <option value="CVEN">CVEN</option>          
+                    <option value="CVEN">CVEN</option>
                     <option value="CSCE">CSCE</option>
                     <option value="CPEN">CPEN</option>
-                    <option value="ECEN">ECEN</option>     
+                    <option value="ECEN">ECEN</option>
                     <option value="EVEN">EVEN</option>
                     <option value="ESET">ESET</option>
-                    <option value="ISEN">ISEN</option>  
+                    <option value="ISEN">ISEN</option>
                     <option value="IDIS">IDIS</option>
                     <option value="MSEN">MSEN</option>
                     <option value="MEEN">MEEN</option>
-                    <option value="NUEN">NUEN</option>    
+                    <option value="NUEN">NUEN</option>
                     <option value="OCEN">OCEN</option>
                     <option value="PETE">PETE</option>
                     {/* These disabled options exist so someone can scroll 
                     to the bottom of the list without it cuttign off*/}
                     <option disabled={true} value="NO"></option>
                     <option disabled={true} value="NO"></option>
-                    <option disabled={true} value="NO"></option>          
+                    <option disabled={true} value="NO"></option>
                 </Select>
-                <Button backgroundColor='red.900' color='white'>Join Queue</Button>
-            </div>
+                <Button isDisabled={!canSubmit} backgroundColor='red.900' color='white'>Join Queue</Button>
+            </Stack>
         </div>
     );
 }
 
-const loginFormStyle = {
-    marginTop: '5%',
-    width: '75vw',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems:'center',
+function isValidPhone(phoneNumber: string) {
+    // Define a regular expression for a U.S. phone number format
+    const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/;
+
+    // Test the phone number against the regular expression
+    return phoneRegex.test(phoneNumber);
 }
