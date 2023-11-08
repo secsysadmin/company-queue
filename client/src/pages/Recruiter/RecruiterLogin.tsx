49a0ad8742b5b7bb39d928b719bc88259cb4d185
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-11-08 15:47:13 -0600

    add recruiter login page

--- client/src/pages/Recruiter/RecruiterLogin.tsx
+++ client/src/pages/Recruiter/RecruiterLogin.tsx
@@ -1,4 +1,30 @@
+import { Card, CardHeader, Stack, Heading, CardBody, Input, Button } from "@chakra-ui/react";
+import Banner from "../../components/Banner";
 
-export default function RecruiterLogin(){
-    return <></>;
+export default function RecruiterLogin() {
+    return <>
+        <Banner title='Recruiter Login'></Banner>
+        {/*@ts-ignore */}
+        <div style={loginDiv}>
+            <Card>
+                <CardHeader>
+                    <Heading size={'lg'}>Enter Company Pin</Heading>
+                </CardHeader>
+                <CardBody>
+                    <Stack>
+                        <Input placeholder="PIN (ex: 123456)"></Input>
+                        <Button backgroundColor={'red.900'} color={'white'}>Login</Button>
+                    </Stack>
+                </CardBody>
+            </Card>
+        </div>
+    </>;
+}
+
+
+const loginDiv = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '3em',
 }
