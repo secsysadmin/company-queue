9c72454f6d56a9e50cb5a64c65ebbcd5d785eedb
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-11-13 12:31:25 -0600

    add recruiter login UI

--- client/src/pages/Recruiter/RecruiterLogin.tsx
+++ client/src/pages/Recruiter/RecruiterLogin.tsx
@@ -1,4 +1,30 @@
+import Banner from "../../components/Banner";
+import { Stack, Input, Button, Text } from '@chakra-ui/react';
+import { useEffect, useState } from "react";
 
-export default function RecruiterLogin(){
-    return <></>;
+const PIN_LENGTH = 6;
+
+export default function RecruiterLogin() {
+    const [canSubmit, setCanSubmit] = useState(false);
+    const [pin, setPin] = useState('');
+
+    useEffect(() => {
+        if(pin.length == PIN_LENGTH){
+            setCanSubmit(true);
+        }
+        else{
+            setCanSubmit(false);           
+        }
+    }, [pin]);
+
+    return <>
+        <div style={{ display: "flex", flexDirection: 'column', alignItems: 'center', minWidth:'50vw' }}>
+            <Banner title='Recruiter Login'></Banner>
+            <Stack minWidth='30vw'>
+                <Text marginTop='3em'>Enter Your Company Queue Pin</Text>
+                <Input variant='filled' placeholder="Pin" onChange={(ev) => setPin(ev.target.value)}></Input>
+                <Button isDisabled={!canSubmit} backgroundColor='red.900' color='white'>Login</Button>
+            </Stack>
+        </div>
+    </>;
 }
