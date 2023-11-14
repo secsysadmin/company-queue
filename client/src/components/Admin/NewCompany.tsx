e58aefff6ee6689ca11edf482d8d374f53d03edd
Sloan Davis <31191957+SloanBDavis@users.noreply.github.com>
2023-11-13 23:31:22 -0600

    add new company ui to admin page

--- client/src/components/Admin/NewCompany.tsx
+++ client/src/components/Admin/NewCompany.tsx
@@ -1,7 +1,19 @@
+import { Card, Text, Input, Button, Stack, CardHeader } from "@chakra-ui/react";
+import { useState } from "react";
 
+export default function NewCompany() {
 
-export default function NewCompany(){
-    return (<>
-
-    </>);
+    return (<div>
+        <Card>
+            <CardHeader>
+            <Text size={'lg'}>Add a new company</Text>
+            </CardHeader>
+            <Stack>
+                <Input placeholder="Company Name"></Input>
+                <Input placeholder="Booth"></Input>
+                <Input placeholder="Pin"></Input>
+                <Button>Submit</Button>
+            </Stack>
+        </Card>
+    </div>);
 }
--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,7 +1,9 @@
-
+import NewCompany from "../../components/Admin/NewCompany";
+import Banner from "../../components/Banner";
 
 export default function Admin() {
     return (<>
-        
+        <Banner title='Admin Panel'></Banner>
+        <NewCompany></NewCompany>
     </>);
 }
