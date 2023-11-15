e8fc78c353658bbbd81322d53f6e7804d45ec241
coopercalk <123708942+coopercalk@users.noreply.github.com>
2023-11-15 16:27:22 -0600

    newqueue component

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,9 +1,22 @@
 import NewCompany from "../../components/Admin/NewCompany";
+import NewQueue from "../../components/Admin/NewQueue";
 import Banner from "../../components/Banner";
 
 export default function Admin() {
     return (<>
-        <Banner title='Admin Panel'></Banner>
-        <NewCompany/>
+        {/*@ts-ignore*/}
+
+        <div style={statusDivStyle}>
+
+            <Banner title='Admin Panel'></Banner>
+            <NewCompany />
+            <NewQueue /></div>
     </>);
 }
+
+const statusDivStyle = {
+    marginTop: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+};
