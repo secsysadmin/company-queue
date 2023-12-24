af45212703381085f019603c93a051a6662fa130
Shaham Noorani <shahamnoorani@gmail.com>
2023-12-24 17:55:26 -0600

    Enable deployments (#1)
    * updated package json for top level node package, update server to serve built client, and update axios to access the write api route

--- client/src/components/LocationRestrictor.tsx
+++ client/src/components/LocationRestrictor.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode, useEffect, useState } from 'react';
-import LocationError from '../pages/LocationError';
+import React, { ReactNode, useEffect, useState } from "react";
+import LocationError from "../pages/LocationError";
 
 interface Location {
   latitude: number;
@@ -12,49 +12,68 @@ interface LocationRestrictorProps {
   radius: number; // in meters
 }
 
-const LocationRestrictor: React.FC<LocationRestrictorProps> = ({ children, targetLocation, radius }) => {
-    const [isWithinRadius, setIsWithinRadius] = useState<boolean | null>(null);
-  
-    useEffect(() => {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
+const LocationRestrictor: React.FC<LocationRestrictorProps> = ({
+  children,
+  targetLocation,
+  radius,
+}) => {
+  const [isWithinRadius, setIsWithinRadius] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
           const distance = calculateDistance(
-            { latitude: position.coords.latitude, longitude: position.coords.longitude },
+            {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            },
             targetLocation
           );
           setIsWithinRadius(distance <= radius);
-        }, () => {
+        },
+        () => {
           // Handle error or set default behavior if location access is denied
           setIsWithinRadius(false);
-        });
-      } else {
-        // Geolocation is not supported by this browser
-        setIsWithinRadius(false);
-      }
-    }, [targetLocation, radius]);
-  
-    if (isWithinRadius === null) {
-      return <div>Loading...</div>;
+        }
+      );
+    } else {
+      // Geolocation is not supported by this browser
+      setIsWithinRadius(false);
     }
-  
-    return isWithinRadius ? <>{children}</> : LocationError();
-  };
-  
-  // Haversine formula to calculate the distance between two points on the Earth
-  function calculateDistance(location1: Location, location2: Location): number {
-    const toRad = (value: number) => (value * Math.PI) / 180;
-    const earthRadius = 6371e3; // meters
-  
-    const dLat = toRad(location2.latitude - location1.latitude);
-    const dLon = toRad(location2.longitude - location1.longitude);
-    const a = 
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(location1.latitude)) * Math.cos(toRad(location2.latitude)) * 
-      Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  
-    return earthRadius * c;
+  }, [targetLocation, radius]);
+
+  // ignore location restrictions in development or if flagged off
+  if (
+    import.meta.env.DEV ||
+    import.meta.env.VITE_LOCATION_RESTRICTIONS === "off"
+  ) {
+    return <>{children}</>;
   }
-  
-  export default LocationRestrictor;
-  
+
+  if (isWithinRadius === null) {
+    return <div>Loading...</div>;
+  }
+
+  return isWithinRadius ? <>{children}</> : LocationError();
+};
+
+// Haversine formula to calculate the distance between two points on the Earth
+function calculateDistance(location1: Location, location2: Location): number {
+  const toRad = (value: number) => (value * Math.PI) / 180;
+  const earthRadius = 6371e3; // meters
+
+  const dLat = toRad(location2.latitude - location1.latitude);
+  const dLon = toRad(location2.longitude - location1.longitude);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRad(location1.latitude)) *
+      Math.cos(toRad(location2.latitude)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return earthRadius * c;
+}
+
+export default LocationRestrictor;
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,14 +3,28 @@ import cors from "cors";
 
 import router from "./routers/router";
 import connectToDatabase from "./db/db";
+import path from "path";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", router);
+
+const extraPath = process.env.PROD ? "../" : "";
+const pathToClentBuild = path.join(__dirname, extraPath + "../../client/dist");
+
+app.use(
+  express.static(pathToClentBuild, {
+    extensions: ["html", "js", "mjs"],
+  })
+);
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(pathToClentBuild, "index.html"));
+});
 
 app.listen(port, async () => {
   await connectToDatabase();
