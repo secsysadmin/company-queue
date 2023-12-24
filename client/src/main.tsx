cd86d6a2bbe70d9399917e2777910998b018a0a8
Shaham Noorani <shahamnoorani@gmail.com>
2023-12-24 16:41:57 -0600

    updated package json for top level node package, update server to serve built client, and update axios to access the write api route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,12 +9,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import { router } from "./routes/router";
 
-import LocationRestrictor from './components/LocationRestrictor';
+import LocationRestrictor from "./components/LocationRestrictor";
 
 const targetLocation = { latitude: 33.03, longitude: -97.08 };
-const radius = 5000;
- 
-axios.defaults.baseURL = "http://localhost:3001/api";
+const radius = 1000000000;
+
+axios.defaults.baseURL = import.meta.env.PROD
+  ? "/api"
+  : "http://localhost:3001/api";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import router from "./routers/router";
 import connectToDatabase from "./db/db";
+import path from "path";
 
 const app = express();
 const port = 3001;
@@ -11,6 +12,19 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", router);
+
+const extraPath = process.env.PROD ? "../" : "";
+const pathToClentBuild = path.join(__dirname, extraPath + "../../client/dist");
+
+app.use(
+  express.static(pathToClentBuild, {
+    extensions: ["html", "js", "mjs"],
+  })
+);
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(pathToClentBuild, "index.html"));
+});
 
 app.listen(port, async () => {
   await connectToDatabase();
