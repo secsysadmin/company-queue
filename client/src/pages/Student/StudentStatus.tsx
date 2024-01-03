9c3d4fbfa5b645d575a08fc4a585565d36ee1c9c
Cooper Calk <coopercalk@tamu.edu>
2024-01-02 20:03:12 -0600

    UX changes cooper (#5)
    * fixed recruiter dashboard
    
    * leave confirmation and info box

--- client/src/pages/Student/StudentStatus.tsx
+++ client/src/pages/Student/StudentStatus.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import {
@@ -10,6 +10,13 @@ import {
   Heading,
   Button,
   Box,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 import Banner from "../../components/Banner";
@@ -17,6 +24,9 @@ import axios from "axios";
 
 export default function StudentStatus() {
   const navigate = useNavigate();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef(null);
 
   const searchParams = new URLSearchParams(location.search);
 
@@ -72,6 +82,20 @@ export default function StudentStatus() {
             </Text>
             Queue
           </Heading>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            textAlign="center"
+            backgroundColor="gray.100"
+            padding="4"
+            borderRadius="md"
+            marginTop="4"
+          >
+            <Text fontSize="md">
+              If your wait time permits, feel free to talk to other companies while you wait.
+            </Text>
+          </Box>
           <Card backgroundColor={"gray.100"}>
             <CardHeader>
               <Heading color={"black"} size={"md"}>
@@ -167,12 +191,45 @@ export default function StudentStatus() {
             </CardBody>
           </Card>
           <Button
-            onClick={() => leaveQueue()}
+            onClick={onOpen}
             backgroundColor={"red.900"}
             color={"white"}
           >
             Leave Queue
           </Button>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent maxWidth={"80vw"}>
+                <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                  Leave Queue
+                </AlertDialogHeader>
+
+                <AlertDialogBody>
+                  Are you sure you want to leave the queue?
+                </AlertDialogBody>
+
+                <AlertDialogFooter>
+                  <Button ref={cancelRef} onClick={onClose}>
+                    No
+                  </Button>
+                  <Button
+                    colorScheme="red"
+                    onClick={() => {
+                      leaveQueue();
+                      onClose();
+                    }}
+                    ml={3}
+                  >
+                    Yes
+                  </Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
         </Stack>
       </Box>
     </>
