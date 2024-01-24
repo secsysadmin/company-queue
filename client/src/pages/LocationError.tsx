bab6465280d4c39bedb75dc79d03daedbb5eed68
coopercalk <123708942+coopercalk@users.noreply.github.com>
2024-01-23 18:12:00 -0600

    Add error message and queue restriction text

--- client/src/pages/LocationError.tsx
+++ client/src/pages/LocationError.tsx
@@ -8,6 +8,7 @@ export default function ErrorPage() {
             <Center mt={"2vh"} marginRight={"6vw"} marginLeft={"6vw"}>
                 <Text fontSize="xl" color="gray.600">
                     Please make sure you are at the Career Fair and have location services enabled to use Company Queue.
+                    If it is not working or there was not a location pop up, make sure your browser location settings set to ask for location permission.
                 </Text>
             </Center>
         </Box>
