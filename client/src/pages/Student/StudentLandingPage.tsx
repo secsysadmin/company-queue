966ebdd82f0cad6e11c21cbb5c3f9ae9d9a717e6
coopercalk <123708942+coopercalk@users.noreply.github.com>
2024-01-25 07:54:17 -0600

    student message

--- client/src/pages/Student/StudentLandingPage.tsx
+++ client/src/pages/Student/StudentLandingPage.tsx
@@ -14,7 +14,7 @@ export default function StudentLandingPage() {
         height="90vh"
       >
         <Text textAlign="center" fontSize="lg">
-          Find a participating company and scan their company queue QR to join a queue
+          Find a participating company and scan their company queue QR to join a queue.<br/><br/>If you have been redirected here after joining a queue, you are in the queue and will recieve a text when it is your turn in line.
         </Text>
       </Box>
     </>
